200781880f38395b5035c73232deb51ccfd58533
thebarmy <souiyahk99@gmail.com>
2023-08-24 15:13:03 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * 0n error, -1 is returne, and err is set appropriately
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
